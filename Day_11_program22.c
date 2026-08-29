cf4c31086a49375c82a8037035cca0f8b7fc6189
Aryan Kumar Singh <aryan.32413@stu.upes.ac.in>
2026-08-29 16:28:51 +0530

    Day 11: to find profit or loss

---
+++ Day_11_program22.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    float cp, sp, profit, loss, percentage;
+
+    printf("Enter cost price: ");
+    scanf("%f", &cp);
+
+    printf("Enter selling price: ");
+    scanf("%f", &sp);
+
+    if (sp > cp)
+    {
+        profit = sp - cp;
+        percentage = (profit / cp) * 100;
+        printf("Profit = %.2f\n", profit);
+        printf("Profit percentage = %.2f%%", percentage);
+    }
+    else if (cp > sp)
+    {
+        loss = cp - sp;
+        percentage = (loss / cp) * 100;
+        printf("Loss = %.2f\n", loss);
+        printf("Loss percentage = %.2f%%", percentage);
+    }
+    else
+    {
+        printf("No profit, no loss");
+    }
+
+    return 0;
+}
